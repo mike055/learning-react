645abda4f5030c6f2aa779f7ea1d48d5b5188d09
michael.blackley <michael.blackley@live.com.au>
2016-11-16 19:34:03 +1100

    facebook tutorial part 2, state and props

--- src/facebook-example/FilterableProductTable.js
+++ src/facebook-example/FilterableProductTable.js
@@ -4,11 +4,36 @@ import SearchBar from './SearchBar';
 import './Products.css';
 
 class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+    
+    this.handleUserInput = this.handleUserInput.bind(this);
+  }
+
+  handleUserInput(filterText, inStockOnly) {
+    this.setState({
+      filterText: filterText,
+      inStockOnly: inStockOnly
+    });
+  }
+
   render() {
     return (
       <div className="products-container">
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+          onUserInput={this.handleUserInput}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
